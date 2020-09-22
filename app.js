f76b307a3747657e94dc2bf2da33233ca6e69f57
SeryeongK <99seryoung@naver.com>
2020-09-22 18:21:57 +0900

    Day 8

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+import helmet from "helmet";
 import express from "express";
 // const express = require('express'); <- 와 같음
 // import express
 import morgan from "morgan";
-import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -12,11 +13,13 @@ import routes from "./routes";
 
 const app = express();              // app variable에 선언해서 express 실행
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);       // url로 들어갈 시에 함수 전체를 사용
---
+++ middlewares.js
@@ -0,0 +1,7 @@
+import routes from "./routes";
+
+export const localsMiddleware = (req, res, next) => {
+    res.locals.siteName = "WeTube";
+    res.locals.routes = routes;    // 뒤의 routes가 impot한 routes
+    next();
+};
